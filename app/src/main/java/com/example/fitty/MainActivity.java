6358d39797a08d872beb207cdbbbbc9cbcff3b6e
inesmarca <imarcarian@itba.edu.ar>
2020-11-10 16:00:54 -0300

    top bar y bottom bar

--- app/src/main/java/com/example/fitty/MainActivity.java
+++ app/src/main/java/com/example/fitty/MainActivity.java
@@ -5,18 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.fitty.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        bottomNavigationView=findViewById(R.id.bottomNav);
+         */
 
     }
 }
