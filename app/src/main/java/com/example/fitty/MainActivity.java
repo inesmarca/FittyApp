830c039e1d16a8baddfd48fd98dc9a0704fc4b30
inesmarca <imarcarian@itba.edu.ar>
2020-11-10 17:58:52 -0300

    fragments hechos falta el titulo al principio

--- app/src/main/java/com/example/fitty/MainActivity.java
+++ app/src/main/java/com/example/fitty/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.fitty;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.fitty.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,14 +21,40 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        /*
+        setContentView(R.layout.activity_main);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         bottomNavigationView=findViewById(R.id.bottomNav);
-         */
-
+        bottomNavigationView.setOnNavigationItemSelectedListener(bottomNavMethod);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new Home()).commit();
     }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener bottomNavMethod = new
+            BottomNavigationView.OnNavigationItemSelectedListener() {
+
+                Fragment fragment = null;
+                @SuppressLint("NonConstantResourceId")
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.progress:
+                            fragment = new Progress();
+                            break;
+                        case R.id.rutines:
+                            fragment = new Rutines();
+                            break;
+                        case R.id.favorites:
+                            fragment = new Favoritas();
+                            break;
+                        default:
+                            fragment = new Home();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+                    binding.topAppBar.setTitle(item.getTitle());
+                    return true;
+                }
+            };
 }
